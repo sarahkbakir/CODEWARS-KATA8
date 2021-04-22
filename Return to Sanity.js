92dfb71dee542380814dffe311465ec9d223c956
Sarah Bakir <sarahbakir.k@gmail.com>
2021-04-22 23:01:29 +0200

    fixed - var

--- Return to Sanity.js
+++ Return to Sanity.js
@@ -1,7 +1,7 @@
 // This function should return an object, but it's not doing what's intended. What's wrong?
 
 function mystery() {
-    var results =
+    let results =
       {sanity: 'Hello'};
     return results;
   }

d4fbcb80edbdf72e3f40b84bcdd62d309cdc83af
Sarah Bakir <sarahbakir.k@gmail.com>
2021-04-22 21:59:16 +0200

    Geometry Basics - Math.sqrt() - Math.pow()

---
+++ Geometry Basics: Distance between points in 2D.js
@@ -0,0 +1,11 @@
+// This series of katas will introduce you to basics of doing geometry with computers.
+
+// Point objects have x and y attributes (X and Y in C#) attributes.
+
+// Write a function calculating distance between Point a and Point b.
+
+// Tests round answers to 6 decimal places.
+
+function distanceBetweenPoints(a, b) {
+    return  Math.sqrt(Math.pow((b.x - a.x), 2) + Math.pow((b.y - a.y), 2));
+  }

6073ee7279450c5d4bd15449858f8ef3c4307708
Sarah Bakir <sarahbakir.k@gmail.com>
2021-04-22 19:21:47 +0200

    quarter of the year

---
+++ Quarter of the year.js
@@ -0,0 +1,10 @@
+// Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+
+// For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
+
+const quarterOf = (m) => {
+    if(m <= 3) { return 1;}
+      if(m <= 6) {return 2;}
+      if(m <= 9) {return 3;}
+      if(m <= 12) {return 4;}
+    }

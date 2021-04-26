99ce020b7a57c27f4817e50300682dbbc9ac570c
Sarah Bakir <sarahbakir.k@gmail.com>
2021-04-26 14:32:24 +0200

    For twins: 1

---
+++ For Twins: 1. Types.js
@@ -0,0 +1,14 @@
+// Prolog:
+// This kata series was created for friends of mine who just started to learn programming. Wish you all the best and keep your mind open and sharp!
+
+// Task:
+// Write function typeValidation that will accept two parameters: variable and type and check if type of variable is matching type. Return true if types match or false if not.
+
+// Examples:
+// typeValidation(42, "number");   // => true
+// typeValidation("42", "number"); // => false
+
+function typeValidation(variable, type) {
+    if(typeof variable === type) {return true}
+    else {return false}
+  }

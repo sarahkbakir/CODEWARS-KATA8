9450556c34d4571bf7e0a32d7a24f41e7047b6b0
Sarah Bakir <sarahbakir.k@gmail.com>
2021-04-26 02:41:23 +0200

    True or false

---
+++ Enumerable Magic #2 - True for Any?.js
@@ -0,0 +1,10 @@
+// Create an any? (JS: any) function that accepts an array and a block (JS: function), and returns true if the block (/function) returns true for any item in the array. If the array is empty, the function should return false.
+
+function any(arr, fun){
+    for(let i = 0; i < arr.length; i++) {
+      if(fun(arr[i])) {
+        return true
+      }
+    }
+    return false
+  }

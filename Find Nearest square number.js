2ab05a094074c144a0a54d95cdeae9459945da7f
Sarah Bakir <sarahbakir.k@gmail.com>
2021-05-03 18:20:58 +0200

    Nearest square number

---
+++ Find Nearest square number.js
@@ -0,0 +1,8 @@
+// Your task is to find the nearest square number, nearest_sq(n), of a positive integer n.
+
+// Goodluck :)
+
+function nearestSq(n){
+    let x = Math.round(Math.sqrt(n))
+    return x **2
+    }

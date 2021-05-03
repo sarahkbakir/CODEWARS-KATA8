cd0d7ef61f8b308480a8cf39b4800fcb7b65ccae
Sarah Bakir <sarahbakir.k@gmail.com>
2021-05-03 21:18:21 +0200

    Blood alkohol content

---
+++ Blood-Alcohol Content.js
@@ -0,0 +1,21 @@
+// Bob drinks too much, and he gets in trouble for it a lot. He drinks so much, in fact, that he has broken the local law enforcement's breathalizer with his alcoholic breath! Bob feels simply aweful, so he wants to make up for it by creating a function that will calculate his blood-alcohol level for them. Unfortunately, Bob has gotten too inebriated to do any programming today, so he needs your help!
+
+// He did manage to research the formula for blood-alcohol content before getting too drunk, which he describes below.
+
+// BAC calculator Formula:
+
+// BAC% = (A × 5.14 / W × r) - .015 × H 
+
+// A: Total alcohol consumed, in ounces (oz)
+// W: Body weight, in pounds (lbs)
+// r: The alcohol distribution ratio, 0.73 for man, and 0.66 for women
+// H: Time passed since drinking, in hours
+
+const bloodAlcoholContent = ({ ounces, abv }, weight, sex, time) =>
+  Number(
+    (
+      ounces * abv * 5.14 / weight * (sex === 'female' ? 0.66 : 0.73) -
+      0.015 * time
+    ).toFixed(4)
+  )
+  
